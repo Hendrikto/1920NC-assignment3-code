fdb8e409496f103bf07673d37c9a3a91511e42a2
Hendrikto <hendrik.to@gmail.com>
2020-03-11 17:57:52 +0100

    Implement function to calculate ROC AUC scores for different parameter configurations

--- src/exercise2.py
+++ src/exercise2.py
@@ -1,8 +1,10 @@
 import subprocess
 
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import (
     auc,
+    roc_auc_score,
     roc_curve,
 )
 
@@ -44,3 +46,15 @@ def plot_roc(y_true, y_pred, n, r):
     plt.legend()
     plt.show()
     plt.close()
+
+
+def calculate_roc_auc_scores(jar_path, alphabet_path, train_path, test_path, y_true, n_max):
+    auc_scores = np.full((n_max,) * 2, np.nan)
+    for n in range(1, n_max + 1):
+        for r in range(1, n + 1):
+            output = run_negsel(jar_path, alphabet_path, train_path, test_path, n, r)
+            scores = aggregate_scores(output)
+            auc_score = roc_auc_score(y_true, scores)
+            auc_scores[n - 1, r - 1] = auc_score
+            print(f'ROC AUC score ({n=}, {r=}): {auc_score:.4f}')
+    return auc_scores
